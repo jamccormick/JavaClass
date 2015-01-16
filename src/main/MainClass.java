0b4d437ac9a297b377f40c73abb8a1b939cd7e14
James McCormick <james.a.mccormick@me.com>
2015-01-16 10:16:22 -0600

    Added looping and discussion of the Primitive/object distinction.

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -3,7 +3,11 @@ package main;
 public class MainClass {
 
 	public static void main(String[] args) {
-		int[] array = {1,2,3,4};
+		//int[] array = {1,2,3,4};
+		Integer[] array = new Integer[4];
+		for(int i = 0; i < array.length; i++){
+			array[i] = i+1;
+		}
 		for (int i : array){
 			StringBuffer outString = new StringBuffer("Hello, World number ");
 			outString.append(i);
