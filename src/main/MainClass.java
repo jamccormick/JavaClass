0b0f2b607e08c6d05cc04b7b8f78c3270fbf77f3
James McCormick <james.a.mccormick@me.com>
2015-01-16 09:38:23 -0600

    Adding some language features to Hello, World

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -3,8 +3,13 @@ package main;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-
+		int[] array = {1,2,3,4};
+		for (int i : array){
+			StringBuffer outString = new StringBuffer("Hello, World number ");
+			outString.append(i);
+			outString.append("!");
+			System.out.println(outString.toString());			
+		}
 	}
 
 }
